e37c3d1a336b195ec04421081bcebcfdfae636bd
BugHunter-X <askzenai@gmail.com>
2025-01-29 03:06:05 +0100

    Add bug.tcl file

---
+++ bug.tcl
@@ -0,0 +1,9 @@
+proc get_value {index} { 
+  if {$index < 0 || $index >= [llength $my_list]} { 
+    return -code error "Index out of range" 
+  } 
+  return [lindex $my_list $index] 
+}
+
+set my_list {1 2 3 4 5}
+puts [get_value 10]
